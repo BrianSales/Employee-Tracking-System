4012aff95bbb52406cb89471f8b853d6acde7fd5
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-04-26 17:13:09 -0500

    did a lot with adding new tables and methods to the code

--- index.js
+++ index.js
@@ -2,81 +2,175 @@ const inquirer = require("inquirer")
 const mysql = require("mysql2")
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Sql26280!',
-  database: 'employee_db'
+    host: 'localhost',
+    user: 'root',
+    password: 'Sql26280!',
+    database: 'employee_db'
 });
 
 let question = [{
-  message: 'What do you want to do?',
-  name: 'action',
-  type: 'list',
-  choices: ["View all departments", "View all roles", "view all employees",
-    "add a department", "add a role", "add an employee", "update employee roles", "end"]
+    message: 'What do you want to do?',
+    name: 'action',
+    type: 'list',
+    choices: ["View all departments", "View all roles", "View all employees",
+        "add a department", "add a role", "add an employee", "update employee roles", "end"]
 }];
 
-// Ask the question
-function start (){
-    inquirer.prompt(question)
-  .then(answer => {
-    if (answer.action === "View all departments") {
-      connection.query('SELECT * FROM departments', function (err, departments) {
+function viewAllDepartments() {
+    connection.query('SELECT * FROM departments', function (err, departments) {
         console.table(departments);
-      })
+        start()
+    });
+}
 
-    } else if (answer.action === 'View all roles') {
-      connection.query('SELECT * FROM roles', function (err, roles) {
+function viewAllRoles() {
+    connection.query('SELECT title, salary, name as Department FROM roles JOIN departments ON roles.department_id = departments.id', function (err, roles) {
         console.table(roles);
-      })
-    } else if (answer.action === 'add a role') {
-      inquirer.prompt([
-        {
-          name: 'title',
-          message: 'Enter the job title:',
-          type: 'input'
-
-        },
-        {
-          name: "salary",
-          message: "What is your salary",
-          type: 'input',
-        }
-
-      ])
-        .then(answer => {
-          connection.query('insert into role (name) values(?)', [answer.department])
-        })
-    }
-
-
-    else if (answer.action === 'add a department') {
-      inquirer.prompt([
-        {
-          name: 'department',
-          message: 'Enter the department name:',
-          type: 'input'
-        }
-      ])
-        .then(answer => {
-          connection.query('insert into departments (name) values(?)', [answer.department])
-        })
-    } 
-    
-    else {
-        console.log("Goodbye")
-        connection.end();
-      }
-
-
-
-
-  })
+        start()
+    })
 
 }
-  // connect to the mysql server and sql database
+
+function viewAllEmployees() {
+    connection.query(`Select 
+    employee.first_name, employee.last_name, title, salary,
+    manager.first_name as manager from employees as employee
+    JOIN roles ON employee.role_id = roles.id
+    LEFT JOIN employees as manager ON
+    employee.manager_id = manager.id`, function(err, employees) {
+        console.table(employees);
+        start();
+    });
+
+}
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            name: 'department',
+            message: 'Enter the department name:',
+            type: 'input'
+        }
+    ])
+        .then(answer => {
+            connection.query('insert into departments (name) values(?)', [answer.department])
+            start()
+        })
+        
+
+}
+
+function addRole() {
+    connection.query('Select * from departments', function (err, departments) {
+        console.log(departments);
+        const departmentsArr = [];
+        departments.forEach(department => {
+            departmentsArr.push(department.name);
+        })
+        inquirer.prompt([
+            {
+                name: 'title',
+                message: 'Enter the job title:',
+                type: 'input'
+
+            },
+            {
+                name: "salary",
+                message: "What is your salary",
+                type: 'input',
+            },
+            {
+                name: "department",
+                message: 'Select the department this role belongs to',
+                type: 'list',
+                choices: departmentsArr
+            }
+        ])
+            .then(answers => {
+                let departmentId = 0;
+                departments.forEach(department => {
+                    if (department.name === answers.department) {
+                        departmentId = department.id;
+                    }
+                })
+                connection.query('insert into roles (title, salary, department_id) values(?, ?, ?)', [answers.title, answers.salary, departmentId])
+                start();
+            })
+    })
+}
+
+function addEmployee(){
+    connection.query('Select id, title from roles', function (err, roles){
+        const rolesArr = [];
+        roles.forEach(role => {
+            rolesArr.push(role.title);
+        })
+        inquirer.prompt([
+            {
+                name: 'title',
+                message: 'Enter the job title:',
+                type: 'input'
+
+            },
+            {
+                name: "salary",
+                message: "What is your salary",
+                type: 'input',
+            },
+            {
+                name: "department",
+                message: 'Select the department this role belongs to',
+                type: 'list',
+                choices: departmentsArr
+            }
+        ])
+
+
+
+    })
+}
+
+// Ask the question
+function start() {
+    inquirer.prompt(question)
+        .then(answer => {
+            if (answer.action === "View all departments") {
+                viewAllDepartments();
+            } else if (answer.action === 'View all roles') {
+                viewAllRoles();
+
+            } else if (answer.action === 'View all employees'){
+                viewAllEmployees();
+            } 
+            else if (answer.action === 'add a role') {
+                addRole();
+
+            } else if(answer.action === 'add an employee'){
+                addEmployee();
+            }
+
+
+            else if (answer.action === 'add a department') {
+                addDepartment();
+            } 
+
+            
+
+            else {
+                console.log("Goodbye")
+                connection.end();
+
+            }
+
+
+
+
+        })
+
+}
+// connect to the mysql server and sql database
 connection.connect((err) => {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
-  });
+});
--- seed.sql
+++ seed.sql
@@ -16,3 +16,27 @@ CREATE TABLE `employee_db`.`departments` (
     REFERENCES `employee_db`.`departments` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+    CREATE TABLE `employee_db`.`employees` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(30) NULL,
+  `last_name` VARCHAR(30) NULL,
+  `role_id` INT NULL,
+  `manager_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_role_idx` (`role_id` ASC) VISIBLE,
+  CONSTRAINT `fk_role`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `employee_db`.`roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+    ALTER TABLE `employee_db`.`employees` 
+ADD INDEX `fk_manager_idx` (`manager_id` ASC) VISIBLE;
+;
+ALTER TABLE `employee_db`.`employees` 
+ADD CONSTRAINT `fk_manager`
+  FOREIGN KEY (`manager_id`)
+  REFERENCES `employee_db`.`employees` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
